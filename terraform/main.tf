b255ced54f303a7ff0c31f5da0e7e88268436636
Wai Yan Phyo <waiyanphyo830@gmail.com>
2025-07-07 19:16:38 +0100

    changing tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,51 +4,40 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "main" {
-  name     = "container-rg"
+  name     = "test-container-rg"
   location = "eastus"
 }
 
 resource "azurerm_virtual_network" "main" {
-  name                = "container-network"
+  name                = "test-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
 
 resource "azurerm_subnet" "internal" {
-  name                 = "internal"
+  name                 = "test-subnet"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "main" {
-  name                = "container-ip"
+  name                = "test-ip"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
-resource "azurerm_network_security_group" "main" {
-  name                = "container-nsg"
+resource "azurerm_network_security_group" "ssh_nsg" {
+  name                = "ssh-nsg"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-
-  security_rule {
-    name                       = "HTTP"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
 
   security_rule {
     name                       = "SSH"
-    priority                   = 110
+    priority                   = 1001
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
@@ -57,10 +46,23 @@ resource "azurerm_network_security_group" "main" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  # Add this new rule for HTTP access
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_network_interface" "main" {
-  name                = "container-nic"
+  name                = "test-nic"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
@@ -72,24 +74,22 @@ resource "azurerm_network_interface" "main" {
   }
 }
 
-resource "azurerm_network_interface_security_group_association" "main" {
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
   network_interface_id      = azurerm_network_interface.main.id
-  network_security_group_id = azurerm_network_security_group.main.id
+  network_security_group_id = azurerm_network_security_group.ssh_nsg.id
 }
 
 resource "azurerm_linux_virtual_machine" "main" {
-  name                = "container-vm"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  size                = "Standard_B1s"
-  admin_username      = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.main.id,
-  ]
+  name                  = "test-vm"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
+  size                  = "Standard_B1s"
+  admin_username        = "azureuser"
+  network_interface_ids = [azurerm_network_interface.main.id]
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("~/.ssh/azure-container-key.pub")
+    public_key = file("C:/Users/User/.ssh/azure-container-key.pub")
   }
 
   os_disk {
@@ -99,10 +99,12 @@ resource "azurerm_linux_virtual_machine" "main" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
     version   = "latest"
   }
+
+  computer_name = "testvm"
 }
 
 output "public_ip" {
