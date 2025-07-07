94b6d15f5898cdf40c5f57064ef97e8c0dd666ee
Wai Yan Phyo <waiyanphyo830@gmail.com>
2025-07-07 23:46:04 +0100

    change cod

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Azure Automated Deployment Success Wai Yan! 🚀"
+    return "Azure Automated Deployment Success Wai Yan Phyo! 🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
